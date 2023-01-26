5a9d90573db19835d76e3c2936410b02a3cecc95
EagleOnGitHub <cappymeta@gmail.com>
2023-01-26 22:55:02 +0200

    avatar command, command framework

--- src/lib.rs
+++ src/lib.rs
@@ -5,19 +5,17 @@ use serenity::model::gateway::Ready;
 use serenity::prelude::*;
 use shuttle_secrets::SecretStore;
 use tracing::{error, info};
+use serenity::framework::standard::macros::{command, group};
+use serenity::framework::standard::{StandardFramework, CommandResult};
 
-struct Bot;
+#[group]
+#[commands(avatar)]
+struct General;
+
+struct Handler;
 
 #[async_trait]
-impl EventHandler for Bot {
-    async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!hello" {
-            if let Err(e) = msg.channel_id.say(&ctx.http, "world!").await {
-                error!("Error sending message: {:?}", e);
-            }
-        }
-    }
-
+impl EventHandler for Handler {
     async fn ready(&self, _: Context, ready: Ready) {
         info!("{} is connected!", ready.user.name);
     }
@@ -27,20 +25,41 @@ impl EventHandler for Bot {
 async fn serenity(
     #[shuttle_secrets::Secrets] secret_store: SecretStore,
 ) -> shuttle_service::ShuttleSerenity {
-    // Get the discord token set in `Secrets.toml`
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("fl."))
+        .group(&GENERAL_GROUP)
+
     let token = if let Some(token) = secret_store.get("DISCORD_TOKEN") {
         token
     } else {
         return Err(anyhow!("'DISCORD_TOKEN' was not found").into());
     };
 
-    // Set gateway intents, which decides what events the bot will be notified about
     let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
 
     let client = Client::builder(&token, intents)
-        .event_handler(Bot)
+        .event_handler(Handler)
+        .framework(framework)
         .await
-        .expect("Err creating client");
+        .expect("Error creating client");
 
     Ok(client)
+}
+
+#[command]
+async fn avatar(ctx: &Context, msg: &Message) -> CommandResult {
+    let error = false;
+    let avatar = match msg.author.avatar_url() {
+        None => {
+            msg.reply(ctx, "Failure acquiring avatar.");
+            error = true;
+        },
+        Some(url) => url,
+    };
+    if error == true {
+        return Err(())
+    }
+    msg.reply(ctx, avatar).await?;
+
+    Ok(())
 }
